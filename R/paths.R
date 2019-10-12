93bac72a0e19a9d67426c9cd798ed22087e8f96f
Minh D. Trinh <mdtrinh@mit.edu>
2019-10-11 22:01:42 -0400

    removed model_objects functionality due to yet unsolved problem with model.frame() and flexible model formula

--- R/paths.R
+++ R/paths.R
@@ -60,8 +60,7 @@ treat <- "democ"
 #######
 
 # First function
-paths <- function(model_objects = NULL,
-                  formulas = NULL,
+paths <- function(formulas = NULL,
                   models = NULL,
                   models_args = NULL,
                   #mediators = NULL,
@@ -70,9 +69,7 @@ paths <- function(model_objects = NULL,
                   #covariates = NULL,
                   outcome = NULL,
                   w = NULL,
-                  data = NULL,
-                  x.train = NULL,
-                  y.train = NULL) {
+                  data = NULL) {
 
   ### parse out function call ###
 
@@ -86,14 +83,12 @@ paths <- function(model_objects = NULL,
                   "wbart")
 
   ### TO-DO: Clean up input data
-  ### Decide on best way to use input data, x.train, y.train etc.
-  ### and keep only observations that common to the full model
-
 
   ## Check to see which input method is used
-  if(is.null(model_objects) & is.null(formulas)) {
-    stop("Either 'model_objects' or 'formulas' must be supplied")
-  } else if(!is.null(formulas)) {
+  if(is.null(formulas)) {
+    stop("'formulas' must be supplied as a list of model formulas")
+
+  } else {
 
     ### Option 1: User input a list of formula and corresponding methods and arguments ###
 
@@ -129,7 +124,7 @@ paths <- function(model_objects = NULL,
     mediators_var <- vector("list", K-1)
     for(i in 1:(K-1)) {
       mediators_var[[i]] <- dplyr::setdiff(all.vars(formulas[[i]]),
-                                  all.vars(formulas[[i + 1]]))
+                                           all.vars(formulas[[i + 1]]))
     }
 
     # Check if treatment name is provided
@@ -194,127 +189,6 @@ paths <- function(model_objects = NULL,
     }
     # output a dataset to use with the bootstrap
     dat_boot <- data
-
-  } else if(!is.null(model_objects)) {
-
-    ## Option 2: User input a list of model objects
-
-    K <- length(model_objects)
-
-    if(!inherits(model_objects, "list")) {
-      stop("'model_objects' must be a list of fitted model objects")
-    }
-    if(!any(sapply(model_objects, function(x) inherits(x, what = c(lm_names, glm_names, bart_names))))) {
-      stop("'model_objects' must be a list containing objects of type 'lm', 'glm' or '*bart'")
-    }
-
-    # Model type indicators
-    isLm <- sapply(model_objects, inherits, lm_names)
-    isGlm <- sapply(model_objects, inherits, glm_names)
-    isBart <- sapply(model_objects, inherits, bart_names)
-
-    ## Retrieve variable names from formula
-
-    # Retrieve outcome name from formula
-    outcome_var <- vector("list", K)
-    for(i in 1:K) {
-      if(isLm[i] | isGlm[i]) {
-        outcome_var[i] <- all.vars(formula(model_objects[[i]]))[1]
-      } else if(isBart[i]) {
-        # not enough information from bart object to identify outcome name
-        outcome_var[i] <- NULL
-      }
-    }
-
-    if(length(unique(unlist(outcome_var))) > 1) {
-      stop("'model_objects' must be a list of models with the same response variable")
-    } else {
-      outcome_var <- unlist(outcome_var)[1]
-    }
-
-    if(is.null(outcome_var)) {
-      if(is.null(outcome)) {
-        warning("No variable name is provided or can be inferred for response variable.
-                Consider setting 'outcome' to a value other than NULL.")
-        outcome_var <- "y"
-      } else {
-        outcome_var <- outcome
-      }
-    }
-
-
-    # Retrieve mediator names from formula
-    mediators_var <- vector("list", K-1)
-    all_vars <- vector("list", K)
-    for(i in K:1) {
-      if(isLm[i] | isGlm[i]) {
-        all_vars[[i]] <- all.vars(formula(model_objects[[i]]))[-1]
-      } else if (isBart[i]) {
-        all_vars[[i]] <- colnames(model_objects[[i]]$varcount)
-        # sanitize column names to get variable names
-        all_vars[[i]] <- sapply(all_vars[[i]], function(v) gsub(".+\\(", "", v))
-        all_vars[[i]] <- sapply(all_vars[[i]], function(v) gsub("\\)", "", v))
-        all_vars[[i]] <- sapply(all_vars[[i]], function(v) gsub("\\^.+", "", v))
-      }
-
-      if(i < K){
-        mediators_var[[i]] <- dplyr::setdiff(all_vars[[i]],
-                                             all_vars[[i+1]])
-      }
-    }
-
-    treat_var <- vector("character", K)
-
-    # Check if treatment name is provided
-    if(is.null(treat)) {
-      stop("'treat' must be specified")
-    } else {
-      if(!treat %in% Reduce(intersect, all_vars)) {
-        stop("'treat' must be specified in every formula")
-      } else {
-        # check for variable in dataset with name that matches treat
-        # accounting for cases when treat is input flexibly e.g. log(treat)
-        treat_var_match <- sapply(all_vars[[1]], function(t) grepl(t, treat, fixed = TRUE))
-        treat_var <- all_vars[[1]][treat_var_match][1]
-      }
-    }
-
-    # Retrieve covariate names from formula
-    covariates_var <- dplyr::setdiff(Reduce(intersect, all_vars), c(treat_var, outcome_var))
-
-    # extract observed outcomes, treatment, mediators and covariates
-    model_y <- model_objects[[1]]
-
-    if(isLm[1] | isGlm[1]) {
-      dat_boot <- model.frame(model_y)
-      names(dat_boot) <- all.vars(formula(model_y))
-
-      dat_boot <- dat_boot[, c(outcome_var, treat_var, unlist(mediators_var), covariates_var)]
-
-    } else if(isBart[1]) {
-      # Verify that the original data are present
-      if(is.null(x.train) | is.null(y.train)) {
-        stop("For BART model_objects, the original x.train and y.train are required")
-      }
-      # Check if the data are at least similar.
-      if(nrow(x.train) != length(y.train)) {
-        stop("The length of y.train and the number of rows in x.train must be identical")
-      }
-      if(nrow(x.train) != ncol(model_y$yhat.train)) {
-        stop("The length of y.train and the number of rows in x.train must match the length of yhat.train in BART model")
-      }
-      if(!all(colnames(model_y$varcount) %in% colnames(x.train))) {
-        stop("x.train must contain every column used in BART model")
-      }
-
-      y <- y.train
-      x <- x.train
-
-      dat_boot <- data.frame(y, x)
-      names(dat_boot) <- c(outcome_var, colnames(x))
-    }
-
-    # output a dataset to use with the bootstrap
 
   }
 
@@ -470,6 +344,7 @@ paths(formulas = list(formula_y, formula_m2),
       y.train = y_mat)
 
 paths(formulas = list(formula_y, formula_m2, formula_m1),
+      models = c("pbart", "pbart"),
       models_args = list(list(sparse = TRUE),
                          list(theta = 0),
                          list(omega = 1)),
